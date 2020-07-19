4fd89436329841afe47b3f306e51e8b81bf0650c
Khairul <e0309879@u.nus.edu>
2020-07-19 17:02:40 +0800

    Changes from command to menu for telegram bot

--- src/backend/telegram/telegram.js
+++ src/backend/telegram/telegram.js
@@ -1,4 +1,5 @@
 const Telegraf = require('telegraf')
+const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
 const axios = require('axios')
 
 require('dotenv').config()
@@ -6,33 +7,79 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const gameShortName = process.env.GAME_SHORT_NAME
 const appEntry = process.env.APP_ENTRY_POINT || 'http://127.0.0.1:5000'
 
-bot.start((ctx) => ctx.reply('Welcome to tPlace!'))
-bot.help(ctx => ctx.reply('Use the /canvas command to start the game!'))
+const menuTemplate = new MenuTemplate(ctx => `Hello ${ctx.update.message.chat.title}! Welcome to tplace!`)
 
-bot.command('canvas', (ctx) =>
+// Configure menu template items
+menuTemplate.interact('Start Drawing', 'draw', {
+    do: (ctx) => {
+        ctx.replyWithGame(gameShortName)
+        return false
+    }
+})
+
+menuTemplate.url('See Canvas', 'https://tww2020.site/tplace.html')
+
+menuTemplate.interact('Whitelist this Chat', 'whitelist', {
+    do: ctx => {
+        const chatId = ctx.update.callback_query.message.chat.id
+        const url = `${appEntry}/whitelist`
+        axios.post(url, {chatId: chatId})
+            .then(res => {
+                if (res.status === 200) {
+                    ctx.reply('This chat group has been successfully whitelisted!')
+                } else {
+                    ctx.reply('Uh oh looks like the whitelist period is up. Contact @Khairoulll for more info')
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                ctx.reply(`Unicorns! Looks like an error has occured. Contact @Khairoulll for more info`)
+            })
+        return false
+    }
+})
+
+menuTemplate.interact('F', 'respect', {
+    do: ctx => {
+        ctx.reply('Your respect has been forwarded to the creators')
+        const message = `${ctx.update.callback_query.from.first_name} has paid us compliments`
+        ctx.telegram.sendMessage(-484684580, message)
+        return false
+    }
+})
+
+
+const menuMiddleware = new MenuMiddleware('/', menuTemplate)
+bot.command('start', ctx => menuMiddleware.replyToContext(ctx))
+bot.use(menuMiddleware)
+
+// configure commands
+bot.help(ctx => ctx.reply('Use the /start command to start the game!'))
+
+bot.command('tplace', (ctx) =>
     ctx.replyWithGame(gameShortName))
 
-bot.command('whitelist', (ctx) => {
-    const chatId = ctx.update.message.chat.id
-    const url = `${appEntry}/whitelist`
-    axios.post(url, {chatId: chatId})
-        .then(res => {
-            if (res.status === 200) {
-                ctx.reply('This chat group has been successfully whitelisted!')
-            } else {
-                ctx.reply('Uh oh looks like the whitelist period is up. Contact @Khairoulll for more info')
-            }
-        })
-        .catch(err => {
-            ctx.reply(`Unicorns! Looks like an error has occured. Contact @Khairoulll for more info`)
-        })
-})
+// bot.command('whitelist', (ctx) => {
+//     const chatId = ctx.update.message.chat.id
+//     const url = `${appEntry}/whitelist`
+//     axios.post(url, {chatId: chatId})
+//         .then(res => {
+//             if (res.status === 200) {
+//                 ctx.reply('This chat group has been successfully whitelisted!')
+//             } else {
+//                 ctx.reply('Uh oh looks like the whitelist period is up. Contact @Khairoulll for more info')
+//             }
+//         })
+//         .catch(err => {
+//             ctx.reply(`Unicorns! Looks like an error has occured. Contact @Khairoulll for more info`)
+//         })
+// })
 
-bot.command('f', (ctx) => {
-    ctx.reply('Your respect has been forwarded to the creators')
-    const message = `${ctx.update.message.from.first_name} has paid us compliments`
-    ctx.telegram.sendMessage(-484684580, message)
-})
+// bot.command('f', (ctx) => {
+//     ctx.reply('Your respect has been forwarded to the creators')
+//     const message = `${ctx.update.message.from.first_name} has paid us compliments`
+//     ctx.telegram.sendMessage(-484684580, message)
+// })
 
 
 bot.on('callback_query', ctx => {
@@ -40,7 +87,6 @@ bot.on('callback_query', ctx => {
     if(!query || query.game_short_name !== gameShortName) {
         ctx.answerCbQuery("Sorry, '" + query.game_short_name + "' is not available.");
     } else {
-        console.log(query)
         const chatId = query.message.chat.id
         const userId = query.from.id
         let gameurl = `${appEntry}/${chatId}/${userId}`;
