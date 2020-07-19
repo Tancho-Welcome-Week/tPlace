0f58d5ee4b424d07e8825a4165958014c732096f
Sam-limyr <nacuniel@gmail.com>
2020-07-19 17:09:46 +0800

    Create RedisManager class with required methods.

--- src/backend/redis_js/canvas.js
+++ src/backend/redis_js/canvas.js
@@ -1,26 +1,111 @@
 const redis = require("redis");
-const config = require("./commons.js");
+const commons = require("./commons.js");
 
 
-function getValue(redisClient, key) {
-    return redisClient.get(key, redis.print);
+class RedisManager {
+    constructor(key) {
+        this.key = key;
+        this.redisClient = redis.createClient(commons.REDIS_CONFIG_FILE);
+    }
+
+    initializeCanvas(canvas_width, canvas_height, pixel_format) {
+        this.width = canvas_width;
+        this.height = canvas_height;
+        this.format = pixel_format;
+        this.setAreaValue(1, 1, this.width, this.height, commons.Color.WHITE);
+    }
+
+    getValue(pixelXCoordinate, pixelYCoordinate) {
+        const offset = commons.calculateOffset(pixelXCoordinate, pixelYCoordinate, this.width);
+        this.redisClient.send_command("bitfield",[this.key, 'GET', this.format, offset], redis.print);
+            // (error, result) => callback(error, result));
+    }
+
+    setValue(pixelXCoordinate, pixelYCoordinate, value) {
+        const offset = commons.calculateOffset(pixelXCoordinate, pixelYCoordinate, this.width);
+        this.redisClient.send_command("bitfield",[this.key, 'SET', this.format, offset,
+            parseInt(value, 2)]);
+    }
+
+    setAreaValue(topLeftXCoordinate, topLeftYCoordinate, bottomRightXCoordinate, bottomRightYCoordinate, color) {
+        for (let y = topLeftYCoordinate; y < bottomRightYCoordinate + 1; y++) {
+            for (let x = topLeftXCoordinate; x < bottomRightXCoordinate + 1; x++) {
+                this.setValue(x, y, color);
+            }
+        }
+    }
+
+    getCanvas() {
+        return new Promise((ok, error) => {
+            this.redisClient.get(new Buffer.from(this.key), (err, result) => {
+                let uint8Array = new Uint8Array(result);
+                // let hexString = res.toString('hex');
+                // let binString = hex2bin(hexString);
+                if (err) {
+                    error(err);
+                } else {
+                    ok(uint8Array);
+                }
+            });
+        }).catch((error) => { console.log(error) });
+    }
+
+    deleteCanvas() {
+        this.redisClient.del(this.key);
+    }
 }
 
-function setValue(redisClient, key, value) {
-    redisClient.set(key, value, redis.print);
-}
 
-const CONFIG_FILE = config.REDIS_CONFIG_FILE;
+const redisManager = new RedisManager(commons.CANVAS_NAME);
+redisManager.initializeCanvas(commons.CANVAS_WIDTH, commons.CANVAS_HEIGHT, commons.PIXEL_FORMAT);
+redisManager.setValue(2, 2, commons.Color.RED);
+redisManager.setValue(1, 1, commons.Color.BLUE);
+// redisManager.getValue();
+redisManager.getCanvas().then((result) => { console.log(result) });
+redisManager.deleteCanvas();
 
-module.exports = { getValue, setValue, CONFIG_FILE };
+
+module.exports = { RedisManager };
 
 
+// const redisClient = redis.createClient(commons.REDIS_CONFIG_FILE);
+// initializeCanvas(redisClient, commons.CANVAS_WIDTH, commons.CANVAS_HEIGHT, commons.CANVAS_NAME);
+// setValue(redisClient, commons.CANVAS_NAME, 1, 1, '0001');
+// setValue(redisClient, commons.CANVAS_NAME, 2, 1, '0010');
+// setValue(redisClient, commons.CANVAS_NAME, 3, 1, '0011');
+// setValue(redisClient, commons.CANVAS_NAME, 1, 2, '0100');
+// setValue(redisClient, commons.CANVAS_NAME, 2, 2, '0101');
+// setValue(redisClient, commons.CANVAS_NAME, 3, 2, '0110');
+// setValue(redisClient, commons.CANVAS_NAME, 1, 3, '0111');
+// setValue(redisClient, commons.CANVAS_NAME, 2, 3, '1000');
+// setValue(redisClient, commons.CANVAS_NAME, 3, 3, '1001');
+// setValue(redisClient, commons.CANVAS_NAME, 4, 4, '1111');
+// for (let i = 0; i < (commons.CANVAS_WIDTH * commons.CANVAS_HEIGHT); i++) {
+//     getValue(redisClient, commons.CANVAS_NAME, '#'+i);
+// }
 
-const redisClient = redis.createClient(config.REDIS_CONFIG_FILE);
-setValue(redisClient, "ping", "pung");
-getValue(redisClient, "ping");
-setValue(redisClient, "ping", "pong");
-getValue(redisClient, "ping");
+// getCanvas(redisClient, commons.CANVAS_NAME).then((result) => { console.log(result); });
+//
+// deleteCanvas(redisClient, commons.CANVAS_NAME);
+//
+// function dec2bin(dec){
+//     const binaryString = (dec >>> 0).toString(2);
+//     const paddedString = ("0000" + binaryString).slice(-4);
+//     return paddedString;
+// }
+//
+// function callback(error, result) {
+//     if (error) {
+//         console.log(error);
+//         throw error;
+//     }
+//     console.log(dec2bin(result));
+// }
+//
+// function hex2bin(hex){
+//     return (parseInt(hex, 16).toString(2));
+// }
+
 
 
 
--- src/backend/redis_js/commons.js
+++ src/backend/redis_js/commons.js
@@ -1,7 +1,4 @@
 
-
-
-
 // Use the constant below if running from Docker
 const REDIS_HOST_ADDRESS = "redis";
 
@@ -12,15 +9,35 @@ const REDIS_HOST_PORT = 6379;
 
 const CANVAS_NAME = "tPlace canvas";
 const PIXEL_FORMAT = 'u4';
-const CANVAS_HEIGHT = 5;
-const CANVAS_WIDTH = 5;
+const CANVAS_HEIGHT = 2;
+const CANVAS_WIDTH = 2;
 
 const REDIS_CONFIG_FILE = {
     port      : REDIS_HOST_PORT,
-    host      : REDIS_HOST_ADDRESS_STANDALONE
+    host      : REDIS_HOST_ADDRESS_STANDALONE,
+    return_buffers : true
 };
 
 const Color = {
+    WHITE: "0000",
+    BEIGE: "0001",
+    CREAM: "0010",
+    YELLOW: "0011",
+    ORANGE: "0100",
+    RED: "0101",
+    MAROON: "0110",
+    VIOLET: "0111",
+    INDIGO: "1000",
+    BLUE: "1001",
+    TURQUOISE: "1010",
+    OLIVE: "1011",
+    GREEN: "1100",
+    LIME: "1101",
+    GREY: "1110",
+    BLACK: "1111"
+};
+
+const ColorDecimal = {
     WHITE: 0,
     BEIGE: 1,
     CREAM: 2,
@@ -48,12 +65,19 @@ function convertOneIndexToZeroIndex(indexToConvert) {
 }
 
 /**
- * Converts a 0-indexed value to its 1-indexed counterpart.
- * @param indexToConvert The 0-indexed value to convert.
+ * Calculates the required offset of the chosen pixel, given the x and y coordinates of the pixel. Both coordinates
+ * must be entered in 1-indexed form (i.e. smallest coordinate value is 1, not 0).
+ * @param pixelXCoordinate The x-coordinate of the pixel, 1-indexed.
+ * @param pixelYCoordinate The y-coordinate of the pixel, 1-indexed.
+ * @param canvasWidth The width of the canvas, in pixels.
+ * @returns {string} Returns a formatted offset string for use in Redis.
  */
-function convertZeroIndexToOneIndex(indexToConvert) {
-    return indexToConvert + 1;
+function calculateOffset(pixelXCoordinate, pixelYCoordinate, canvasWidth) {
+    const heightIndexOffset = convertOneIndexToZeroIndex(pixelYCoordinate) * canvasWidth;
+    const widthIndexOffset = convertOneIndexToZeroIndex(pixelXCoordinate);
+    const totalOffset = (heightIndexOffset + widthIndexOffset);
+    return "#" + totalOffset;
 }
 
-module.exports = { REDIS_CONFIG_FILE };
+module.exports = { REDIS_CONFIG_FILE, calculateOffset, Color, CANVAS_HEIGHT, CANVAS_WIDTH, CANVAS_NAME, PIXEL_FORMAT };
 
