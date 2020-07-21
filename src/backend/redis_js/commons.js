4f7fca55fa9596b92dc44f3f8b2ae5f0892df4b7
rohan-av <rohanaryavarma@gmail.com>
2020-07-21 18:00:04 +0800

    Update redis commons file to access keys.js

--- src/backend/redis_js/commons.js
+++ src/backend/redis_js/commons.js
@@ -1,6 +1,6 @@
-
+const keys = require("./keys.js");
 // Use the constant below if running from Docker
-const REDIS_HOST_ADDRESS = "redis";
+const REDIS_HOST_ADDRESS_DOCKER = "redis";
 
 // Use the constant below if running as standalone
 const REDIS_HOST_ADDRESS_STANDALONE = "localhost";
@@ -8,53 +8,54 @@ const REDIS_HOST_ADDRESS_STANDALONE = "localhost";
 const REDIS_HOST_PORT = 6379;
 
 const CANVAS_NAME = "tPlace canvas";
-const PIXEL_FORMAT = 'u4';
+const PIXEL_FORMAT = "u4";
 const CANVAS_HEIGHT = 2;
 const CANVAS_WIDTH = 2;
 
 const REDIS_CONFIG_FILE = {
-    port      : REDIS_HOST_PORT,
-    host      : REDIS_HOST_ADDRESS,
-    // host      : REDIS_HOST_ADDRESS_STANDALONE,
-    return_buffers : true
+  port: REDIS_HOST_PORT,
+  host: keys.redisHost,
+  // host      : REDIS_HOST_ADDRESS,
+  // host      : REDIS_HOST_ADDRESS_STANDALONE,
+  return_buffers: true,
 };
 
 const Color = {
-    WHITE: "0000",
-    BEIGE: "0001",
-    CREAM: "0010",
-    YELLOW: "0011",
-    ORANGE: "0100",
-    RED: "0101",
-    MAROON: "0110",
-    VIOLET: "0111",
-    INDIGO: "1000",
-    BLUE: "1001",
-    TURQUOISE: "1010",
-    OLIVE: "1011",
-    GREEN: "1100",
-    LIME: "1101",
-    GREY: "1110",
-    BLACK: "1111"
+  WHITE: "0000",
+  BEIGE: "0001",
+  CREAM: "0010",
+  YELLOW: "0011",
+  ORANGE: "0100",
+  RED: "0101",
+  MAROON: "0110",
+  VIOLET: "0111",
+  INDIGO: "1000",
+  BLUE: "1001",
+  TURQUOISE: "1010",
+  OLIVE: "1011",
+  GREEN: "1100",
+  LIME: "1101",
+  GREY: "1110",
+  BLACK: "1111",
 };
 
 const ColorDecimal = {
-    WHITE: 0,
-    BEIGE: 1,
-    CREAM: 2,
-    YELLOW: 3,
-    ORANGE: 4,
-    RED: 5,
-    MAROON: 6,
-    VIOLET: 7,
-    INDIGO: 8,
-    BLUE: 9,
-    TURQUOISE: 10,
-    OLIVE: 11,
-    GREEN: 12,
-    LIME: 13,
-    GREY: 14,
-    BLACK: 15
+  WHITE: 0,
+  BEIGE: 1,
+  CREAM: 2,
+  YELLOW: 3,
+  ORANGE: 4,
+  RED: 5,
+  MAROON: 6,
+  VIOLET: 7,
+  INDIGO: 8,
+  BLUE: 9,
+  TURQUOISE: 10,
+  OLIVE: 11,
+  GREEN: 12,
+  LIME: 13,
+  GREY: 14,
+  BLACK: 15,
 };
 
 /**
@@ -62,7 +63,7 @@ const ColorDecimal = {
  * @param indexToConvert The 1-indexed value to convert.
  */
 function convertOneIndexToZeroIndex(indexToConvert) {
-    return indexToConvert - 1;
+  return indexToConvert - 1;
 }
 
 /**
@@ -74,11 +75,19 @@ function convertOneIndexToZeroIndex(indexToConvert) {
  * @returns {string} Returns a formatted offset string for use in Redis.
  */
 function calculateOffset(pixelXCoordinate, pixelYCoordinate, canvasWidth) {
-    const heightIndexOffset = convertOneIndexToZeroIndex(pixelYCoordinate) * canvasWidth;
-    const widthIndexOffset = convertOneIndexToZeroIndex(pixelXCoordinate);
-    const totalOffset = (heightIndexOffset + widthIndexOffset);
-    return "#" + totalOffset;
+  const heightIndexOffset =
+    convertOneIndexToZeroIndex(pixelYCoordinate) * canvasWidth;
+  const widthIndexOffset = convertOneIndexToZeroIndex(pixelXCoordinate);
+  const totalOffset = heightIndexOffset + widthIndexOffset;
+  return "#" + totalOffset;
 }
 
-module.exports = { REDIS_CONFIG_FILE, calculateOffset, Color, CANVAS_HEIGHT, CANVAS_WIDTH, CANVAS_NAME, PIXEL_FORMAT };
-
+module.exports = {
+  REDIS_CONFIG_FILE,
+  calculateOffset,
+  Color,
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  CANVAS_NAME,
+  PIXEL_FORMAT,
+};
