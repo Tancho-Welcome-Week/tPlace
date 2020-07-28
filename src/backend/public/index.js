7bda10f84b8da5627584ca058bd0693ff5ff8f20
zxrycyan <18YSHIH054Z@student.ri.edu.sg>
2020-07-29 04:57:14 +0800

    Changed offscreenCanvas to normal canvas

--- src/backend/public/index.js
+++ src/backend/public/index.js
@@ -48,7 +48,7 @@ function draw() {
     let memCvs = document.createElement('canvas');
     memCvs.width = CANVAS_WIDTH;
     memCvs.height = CANVAS_HEIGHT;
-    // let memCvs = new canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    // let memCvs = new OffscreenCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     let memCtx = memCvs.getContext('2d');
 
     let myImgData = memCtx.createImageData(CANVAS_WIDTH, CANVAS_HEIGHT); // might change to global letiable
