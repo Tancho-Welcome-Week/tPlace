4f7389f0d48f0de8000bc1d63110ae8c7172a8e5
Sahilgat <nisgat@live.com>
2020-08-03 02:33:26 +0800

    Updated init.sql file

--- src/backend/database/init.sql
+++ src/backend/database/init.sql
@@ -3,12 +3,12 @@ drop table if exists Whitelist cascade;
 drop table if exists Canvas cascade;
 
 create table Whitelist (
-    group_id varchar(20) primary key
+    group_id varchar(30) primary key
 );
 
 create table Users (
-    telegram_id varchar(20) primary key,
-    group_id varchar(20) NOT NULL,
+    telegram_id varchar(300) primary key,
+    group_id varchar(30) NOT NULL,
     last_updated timestamp NOT NULL DEFAULT NOW(),
     accumulated_pixels integer DEFAULT 0,
     notifications bool DEFAULT TRUE,
@@ -16,10 +16,9 @@ create table Users (
 );
 
 create table Canvas (
-	telegram_id varchar(20) NOT NULL,
+	telegram_id varchar(300) NOT NULL,
     bitfield bytea,
     last_updated timestamp NOT NULL DEFAULT NOW(),
-    primary key (bitfield, last_updated),
     foreign key (telegram_id) references Users
 );
 
